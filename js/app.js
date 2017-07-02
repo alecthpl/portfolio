0beb36ad2faafeb8cf351132ddf133f3b48931e9
AP <alecthpl@gmail.com>
2017-07-02 15:10:14 -0400

    UI updates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+    // site info popup
+    $('#sites img').on('click', function() {
+        $('#site-bg, #site-info').fadeIn(150);
+    });
+    $('#site-close').on('click', function() {
+        $('#site-bg, #site-info').fadeOut(150);
+    })
+});
