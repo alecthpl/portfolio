2b222c463926508114e1a85836066c2fd8631217
AP <alecthpl@gmail.com>
2017-07-03 18:52:00 -0400

    new design

--- js/app.js
+++ js/app.js
@@ -1,33 +1,11 @@
-$(document).ready(function() {
-    // sites slider
-    var slideCount = $('#sites .row').length;
+// hide hero
+$('#hero .button').on('click', function() {
+    $('#hero').addClass('move-hero');
+    $('#container').addClass('move-container');
 
-    $('#prev').on('click', function() {
-        var current = $('#sites .row.active').data('id');
-
-        if (current == 1) {
-            $('#sites .row').removeClass('active');
-            $('#sites .row[data-id="' + slideCount + '"]').addClass('active');
-            $('#site-count').html(slideCount + '/' + slideCount);
-        } else {
-            $('#sites .row').removeClass('active');
-            $('#sites .row[data-id="' + (current - 1) + '"]').addClass('active');
-            $('#site-count').html((current - 1) + '/' + slideCount);
-        }
-    });
-    $('#next').on('click', function() {
-        var current = $('#sites .row.active').data('id');
-
-        if (current == slideCount) {
-            $('#sites .row').removeClass('active');
-            $('#sites .row[data-id="1"]').addClass('active');
-            $('#site-count').html('1/' + slideCount);
-        } else {
-            $('#sites .row').removeClass('active');
-            $('#sites .row[data-id="' + (current + 1) + '"]').addClass('active');
-            $('#site-count').html((current + 1) + '/' + slideCount);
-        }
-    });
-
+    setTimeout(function() {
+        $('header').css('position', 'fixed');
+        $('body').css('overflow-y', 'auto');
+    }, 600);
 
 });
